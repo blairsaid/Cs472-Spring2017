1c052212c601457438c447a20cd64e69464e3430
EC2 Default User <ec2-user@ip-172-31-12-49.us-west-2.compute.internal>
2017-03-21 19:50:26 +0000

    Added a sample python cgi script.

---
+++ cgi/pythontest.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import cgitb
+cgitb.enable()
+
+print("Content-Type: text/html;charset=utf-8\n")
+print("<!DOCTYPE html>\n")
+print("<html>\n")
+print("<body>\n")
+print("<h1>This is a python script.</h1>\n")
+print("<p>Executed on my amazon ec2 instance.</p>\n")
+print("</body>\n")
+print("</html>\n")
