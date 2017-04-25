91ae8cb5f2e601ec2c1160df455c565f9c1080d1
AaronSargento <aaron.sargento@yahoo.com>
2017-04-25 16:22:49 -0700

    Update LeaderboardData.swift

--- ios/StockNewsFilter/LeaderboardData.swift
+++ ios/StockNewsFilter/LeaderboardData.swift
@@ -16,17 +16,4 @@ class LeaderboardData: NSObject {
     var percentChange: String?
     var weightedavebasicsharesos: String?
     
-    /*
-        This is our constructor for the LeaderboardData class; we will use an array of LeaderboardData objects to fill spreadsheet
-    */
-    /*
-    init(ticker: String, volume: String, price: String, percentChange: String, weightedavebasicsharesos: String) {
-        self.ticker = ticker
-        self.volume = volume
-        self.price = price
-        self.percentChange = percentChange
-        self.weightedavebasicsharesos = weightedavebasicsharesos
-    }
-    */
-    
 }
